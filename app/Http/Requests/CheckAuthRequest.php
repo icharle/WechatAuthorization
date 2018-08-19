dba5ae36f4b1633ecf2823001bbbe73092907ac6
icharle <icharle@live.com>
2018-08-19 15:36:08 +0800

    添加参数验证类

---
+++ app/Http/Requests/CheckAuthRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\UnauthorizedException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class CheckAuthRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'scene' => 'required'
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpException(422, '参数不完整');
+    }
+}
